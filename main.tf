aded49dec915e5ba052118fb8f6cdff60456a18b
Javier Soriano <114910690+sorianfr@users.noreply.github.com>
2025-02-01 08:35:31 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
-provider "aws" {
-  region  = var.aws_region
-  profile = var.aws_profile
+erraform {
+
+        required_providers {
+                aws = {
+                        source = "hashicorp/aws"
+                        version = "~> 4.0"
+                }
+        }
 }
 
 # Create a Shared VPC for Both Clusters
